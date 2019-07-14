a4e4d49c670adb44c54a1370593b72183dd20208
Lindsay Braviak <lbraviak@gmail.com>
2019-07-14 21:01:07 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+phrase = "Hello there, and how are you?"
+array = phrase.split("")
+puts array
+def reverse_each_word(phrase)
+  array = phrase.split("")
+  NewArray = reverse.array()
+  return NewArray()
+end
