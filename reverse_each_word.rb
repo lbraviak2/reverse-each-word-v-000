09d01643c5bb84e425a0f9513c453f425cf24028
Lindsay Braviak <lbraviak@gmail.com>
2019-07-14 21:02:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ phrase = "Hello there, and how are you?"
 array = phrase.split("")
 puts array
 def reverse_each_word(phrase)
+  array = []
   array = phrase.split("")
-  puts array()
+  puts array
 end
